553d9dd21b2eac3d30dd443ecaa5e0099c08dcca
nat <natkuziemka@gmail.com>
2022-11-16 19:58:05 +0100

    projekt

---
+++ js/main.js
@@ -0,0 +1,10 @@
+let name='1';
+console.log(name);
+let name2='2';
+console.log(name2);
+let name3='3';
+console.log(name3)
+let text='tablica';
+console.log(text);
+let array=[1,2,3,] 
+console.log(array)
